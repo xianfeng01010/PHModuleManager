70997d57ba7c2423a38116f59cfbfb8e902e62ef
xinph <xinph@yiche.com>
2023-07-04 01:01:50 +0800

    修改spec文件

--- Example/Pods/Target Support Files/PHModuleManager/PHModuleManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PHModuleManager : NSObject
-@end
-@implementation PodsDummy_PHModuleManager
-@end
--- Example/Pods/Target Support Files/Pods-PHModuleManager_Tests/Pods-PHModuleManager_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PHModuleManager_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PHModuleManager_Tests
-@end
